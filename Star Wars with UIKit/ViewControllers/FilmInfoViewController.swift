6b2517fe00d4ed7e804aa82aaedc2a6d403dc826
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2022-11-01 18:55:40 +0300

    Work on Film Info View Controller

--- Star Wars with UIKit/ViewControllers/FilmInfoViewController.swift
+++ Star Wars with UIKit/ViewControllers/FilmInfoViewController.swift
@@ -25,16 +25,69 @@ final class FilmInfoViewController: UIViewController {
         return image
     }()
     
-    private lazy var openingCrawlLabel: UILabel = {
+    private lazy var episodeIdLabel: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontSizeToFitWidth = true
+        label.lineBreakMode = .byCharWrapping
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var nameKyeEpisodeIid: UILabel = {
+        let label = UILabel()
+        label.text = "Episode: "
         label.translatesAutoresizingMaskIntoConstraints = false
         label.adjustsFontSizeToFitWidth = true
         return label
     }()
     
-    private lazy var nameKyeOpeningCrawl: UILabel = {
+    private lazy var directorLabel: UILabel = {
         let label = UILabel()
-        label.text = "Opening Crawl: "
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontSizeToFitWidth = true
+        label.lineBreakMode = .byCharWrapping
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var nameKyeDirector: UILabel = {
+        let label = UILabel()
+        label.text = "Director: "
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
+    
+    private lazy var producerLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontSizeToFitWidth = true
+        label.lineBreakMode = .byCharWrapping
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var nameKyeProducer: UILabel = {
+        let label = UILabel()
+        label.text = "Producer: "
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
+    
+    private lazy var releaseDateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontSizeToFitWidth = true
+        label.lineBreakMode = .byCharWrapping
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var nameKyeReleaseDate: UILabel = {
+        let label = UILabel()
+        label.text = "Release date: "
         label.translatesAutoresizingMaskIntoConstraints = false
         label.adjustsFontSizeToFitWidth = true
         return label
@@ -52,31 +105,38 @@ final class FilmInfoViewController: UIViewController {
             }
         }
         
-        setupLayout(with: nameKyeOpeningCrawl, and: openingCrawlLabel)
-        openingCrawlLabel.text = film.openingCrawl
+        setupLayout(with: nameKyeEpisodeIid, and: episodeIdLabel, with: 200)
+        setupLayout(with: nameKyeDirector, and: directorLabel, with: 240)
+        setupLayout(with: nameKyeProducer, and: producerLabel, with: 280)
+        setupLayout(with: nameKyeReleaseDate, and: releaseDateLabel, with: 320)
+        
+        episodeIdLabel.text = String(film.episodeId)
+        directorLabel.text = film.director
+        producerLabel.text = film.producer
+        releaseDateLabel.text = film.releaseDate
     }
     
     // MARK: - Private function
     
-    private func setupLayout(with labelOne: UILabel, and labelTwo: UILabel, with constant: CGFloat? = 0) {
+    private func setupLayout(with labelOne: UILabel, and labelTwo: UILabel, with constant: CGFloat) {
         let stackview = UIStackView()
         stackview.axis = .horizontal
         stackview.spacing = 10
         stackview.translatesAutoresizingMaskIntoConstraints = false
         stackview.addArrangedSubview(labelOne)
-        labelOne.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        labelOne.widthAnchor.constraint(equalToConstant: 110).isActive = true
         labelOne.heightAnchor.constraint(equalToConstant: 100).isActive = true
         
         stackview.addArrangedSubview(labelTwo)
-        labelTwo.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        labelTwo.widthAnchor.constraint(equalToConstant: 110).isActive = true
         labelTwo.heightAnchor.constraint(equalToConstant: 100).isActive = true
         
         self.view.addSubview(stackview)
-//        stackview.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: constant ?? 20).isActive = true
+//        stackview.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: constant ?? 0).isActive = true
 //        stackview.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
         stackview.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
-        stackview.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 200).isActive = true
+        stackview.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: constant).isActive = true
         stackview.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 20).isActive = true
         
         self.view.addSubview(filmInfoImage)
