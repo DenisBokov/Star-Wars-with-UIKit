3f0b8e1176c0d35f5d4eff4df8cd0d6aebf29ec0
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2022-11-14 21:46:37 +0300

    + all image characters

--- Star Wars with UIKit/Services/ImageData.swift
+++ Star Wars with UIKit/Services/ImageData.swift
@@ -16,7 +16,13 @@ struct ImageData {
         "Han Solo", "Greedo", "Jabba Desilijic Tiure", "Jabba Desilijic Tiure", "Wedge Antilles", "Jek Tono Porkins",
         "Yoda", "Palpatine", "Boba Fett", "IG-88", "Bossk", "Lando Calrissian", "Lobot", "Ackbar", "Mon Mothma",
         "Arvel Crynyd", "Wicket Systri Warrick", "Nien Nunb", "Qui-Gon Jinn", "Nute Gunray", "Finis Valorum",
-        "Roos Tarpals", "Padmé Amidala", "Jar Jar Binks", "Rugor Nass", "Ric Olié", "Watto", "Sebulba"
+        "Roos Tarpals", "Padmé Amidala", "Jar Jar Binks", "Rugor Nass", "Ric Olié", "Watto", "Sebulba", "Quarsh Panaka",
+        "Shmi Skywalker", "Darth Maul", "Bib Fortuna", "Ayla Secura", "Ratts Tyerel", "Dud Bolt", "Gasgano",
+        "Ben Quadinaros", "Mace Windu", "Ki-Adi-Mundi", "Kit Fisto", "Eeth Koth", "Adi Gallia", "Saesee Tiin",
+        "Yarael Poof", "Plo Koon", "Mas Amedda", "Gregar Typho", "Cordé", "Cliegg Lars", "Poggle The Lesser",
+        "Luminara Unduli", "Barriss Offee", "Dormé", "Dooku", "Bail Prestor Organa", "Jango Fett", "Zam Wesell",
+        "Dexter Jettster", "Lama Su", "Taun We", "Jocasta Nu", "R4-P17", "Wat Tambor", "San Hill", "Shaak Ti",
+        "Grievous", "Tarfful", "Raymus Antilles", "Sly Moore", "Tion Medon"
     ]
     
     static let filmTitles = [
